a81b64ca8c935cb03c853c5972e178aff1939746
Kevin Peng <kevinpkf@gmail.com>
2021-06-29 21:11:48 -0700

    Added user model and empty authentication controller endpoints

---
+++ controllers/authController.js
@@ -0,0 +1,25 @@
+import User from '../models/user.model.js';
+
+export const createUser = async (req, res) => {
+
+}
+
+export const deleteUser = async (req, res) => {
+  
+}
+
+export const getUsers = async (req, res) => {
+  
+}
+
+export const authenticateUser = async (req, res) => {
+  
+}
+
+export const changeUsername = async (req, res) => {
+  
+}
+
+export const changePassword = async (req, res) => {
+  
+}
---
+++ models/user.model.js
@@ -0,0 +1,36 @@
+import mongoose from "mongoose";
+
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    hash: {
+      type: String,
+      required: true,
+    },
+    permissions: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    refresh_token: {
+      type: String,
+      required: true,
+    },
+    access_token: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const User = mongoose.model("User", userSchema);
+export default User;
